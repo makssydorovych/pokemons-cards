9f1a11091f6814372ed5a5673edc3d5401a7225f
Maksym <maksymsydorovych@gmail.com>
2023-04-20 16:35:17 +0200

    add scss files structure, make background of app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./sections/Navbar";
 import Wrapper from "./sections/Wrapper";
 import Footer from "./sections/Footer";
 import Background from "./components/Background";
+import "./scss/index.scss"
 
 const App = () => {
     return (
--- src/sections/Wrapper.tsx
+++ src/sections/Wrapper.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Wrapper = () => {
     return (
         <div>
-            
+            wwww
         </div>
     );
 };
