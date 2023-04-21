9c64d374ed1b7eaaae18bee7c09fe25970e0c65b
Maksym <maksymsydorovych@gmail.com>
2023-04-21 21:08:46 +0200

    add pok types

--- src/components/PokemonCardGrid.tsx
+++ src/components/PokemonCardGrid.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {userPokemonsType} from "../utils/types";
+import {pokemonTypeInterface, userPokemonsType} from "../utils/types";
 
 
 
@@ -11,10 +11,23 @@ const PokemonCardGrid = ({pokemons}: {pokemons: userPokemonsType[]}) => {
                     pokemons && pokemons.length >0 &&
                 pokemons?.map((data:userPokemonsType)=>{
                     return <div className="pokemon-card" key={data.id}>
-                    <div className="pokemon-card-lis"></div>
+                    <div className="pokemon-card-list"></div>
                         <div className="pokemon-card-compare"></div>
                         <h3 className="pokemon-card-title">{data.name}</h3>
-                        <img src={data.image} alt="pokemon image" className="pokemon-card-image"/>
+                        <img src={data.image} alt="pokemon image" className="pokemon-card-image" loading="lazy"/>
+                        <div className="pokemon-card-types">
+                            {data.types.map((type:pokemonTypeInterface, index:number)=>{
+                                const keys = Object.keys(type);
+                                return(
+                                    <div className="pokemon-card-types-type" key={index}>
+                                        <img src={type[keys[0]].image} alt="pokemon type" loading="lazy" className="pokemon-card-types-type-image"/>
+                                        <h6 className="pokemon-card-types-type-text">
+                                            {keys[0]}
+                                        </h6>
+                                    </div>
+                                )
+                            })}
+                        </div>
                     </div>
                 })
                 }
