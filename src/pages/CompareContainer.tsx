ed30a921a32db0e36a4b79c4eb1337e0695e8728
Maksym <maksymsydorovych@gmail.com>
2023-04-24 14:44:01 +0200

    compare pokemons , types

--- src/pages/CompareContainer.tsx
+++ src/pages/CompareContainer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {userPokemonsType} from "../utils/types";
 import {FaPlus} from "react-icons/fa";
-
+import {pokemonTypeInterface} from "../utils/types";
 const CompareContainer = ({pokemon = undefined, isEmpty = false}: { pokemon?: userPokemonsType, isEmpty?: boolean }
 ) =>
 {
@@ -10,12 +10,35 @@ const CompareContainer = ({pokemon = undefined, isEmpty = false}: { pokemon?: us
             {isEmpty &&
             <div className="empty">
             <button><FaPlus/></button>
-                <h4>Add Pokemon</h4>
+                <h3>Add Pokemon</h3>
             </div>
             }
             {
                 pokemon &&
-                <div className="compare-element"></div>
+                <div className="compare-element">
+                        <div className="compare-info">
+                            <div className="compare-details">
+                                <h3>{pokemon?.name}</h3>
+                                <img src={pokemon?.image} alt="pokemon image" className="compare-image"/>
+                            </div>
+                            <div className="pokemon-types container">
+                                <div className="pokemon-types">
+                                    <h4 className="pokemon-type-title">Type</h4>
+                                    <div className="pokemon-type-icons">
+                                        {pokemon?.types.map((type: pokemonTypeInterface)=>{
+                                        const keys = Object.keys(type)
+                                            return (
+                                                <div className="pokemon-type">
+                                            <img src={type[keys[0]].image} alt="type of pokemon"/>
+                                            </div>
+                                            )
+                                        }
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </div>
             }
         </div>
     );
